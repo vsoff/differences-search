6e43d7f9fb91d4a09ba6ffd976fb860c0d63ce45
vsoff <rucamper@mail.ru>
2019-03-28 18:54:24 +0300

    Init commit

---
+++ DifferencesSearch/DifferenceController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DifferencesSearch
+{
+    public interface IDifferenceController
+    {
+        IAutoDifferenceSearchBuilder<T> SetAutoBuilder<T>();
+        ICustomDifferenceSearchBuilder<T> SetCustomBuilder<T>();
+        PropertyDifference[] GetAutoDifferences<T>(T firstValue, T secondValue);
+        PropertyDifference[] GetCustomDifferences<T>(T firstValue, T secondValue);
+    }
+
+    public interface IDifferenceSearchBuilder<TSource>
+    {
+        void Clear();
+        void Build();
+        IAutoDifferenceSearchBuilder<TSource> Stop<TProp>(Expression<Func<TSource, TProp>> expression);
+    }
+
+    public interface IAutoDifferenceSearchBuilder<TSource> : IDifferenceSearchBuilder<TSource>
+    {
+        IAutoDifferenceSearchBuilder<TSource> Ignore<TProp>(Expression<Func<TSource, TProp>> expression);
+    }
+
+    public interface ICustomDifferenceSearchBuilder<TSource> : IDifferenceSearchBuilder<TSource>
+    {
+        IAutoDifferenceSearchBuilder<TSource> Trace<TProp>(Expression<Func<TSource, TProp>> expression);
+    }
+
+    internal class PropertyTreeNode
+    {
+
+    }
+}
---
+++ DifferencesSearch/PropertyDifference.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DifferencesSearch
+{
+    public class PropertyDifference
+    {
+        public Type ClassType { get; set; }
+        public Type FieldType { get; set; }
+        public string PropertyName { get; set; }
+        public object ValueLeft { get; set; }
+        public object ValueRight { get; set; }
+    }
+}
